ed65be5b66615446718563fac199659b97081f4c
Mike Wittenauer <mikewittenauer@me.com>
2024-05-18 09:01:03 -0400

    Add validation to name and description on the family tree model

--- app/models/family_tree.rb
+++ app/models/family_tree.rb
@@ -2,6 +2,9 @@ class FamilyTree < ApplicationRecord
   belongs_to :user
   has_many :people
 
+  validates :name, presence: { message: 'is required' }
+  validates :description, presence: { message: 'is required' }
+
   def member_count
     people.count
   end
