74ed25fe8048a62ebb627612f2c27ba270adbf61
Mike Wittenauer <mikewittenauer@me.com>
2024-06-06 14:18:46 -0400

    Limit the selection of add mother and add father to only family members that make sense

--- app/models/family_tree.rb
+++ app/models/family_tree.rb
@@ -5,6 +5,14 @@ class FamilyTree < ApplicationRecord
   validates :name, presence: { message: 'is required' }
   validates :description, presence: { message: 'is required' }
 
+  def males_older_than(person)
+    people.where(gender: 'male').where("date_of_birth < ?", person.date_of_birth)
+  end
+
+  def females_older_than(person)
+    people.where(gender: 'female').where("date_of_birth < ?", person.date_of_birth)
+  end
+
   def member_count
     people.count
   end
